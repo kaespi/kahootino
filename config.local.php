6936442e962e2d89ecb65a2a48f29060ad38621c
Kaspar Giger <33541015+kaespi@users.noreply.github.com>
2026-03-04 00:29:36 +0100

    Update config.local.php template

--- config.local.php
+++ config.local.php
@@ -6,3 +6,6 @@ $DB_PASS = 'db_password';
 
 $URL_TO_QUIZ = 'https://yourdomain.com/kahootino';
 $DEFAULT_QUIZ_CODE = 'kahootino2026';
+
+$ABLY_API_KEY = '<publish key>';
+$ABLY_CLIENT_KEY = '<subscribe key>';
